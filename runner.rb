e3e0690e10a54a1d5c7f5a3c516757e29c707597
deannawarren <mrs.deanna.warren@gmail.com>
2016-03-18 21:00:39 -0700

    remove unused class until needed

--- runner.rb
+++ runner.rb
@@ -1,8 +1,6 @@
 # running code
-require_relative 'clock.rb'
 require 'csv'
 
-clock = Clock.new
 
 def results
   puts "this will have the results"
@@ -20,7 +18,7 @@ def results
       result_hash[:play] += 1
     when "procrastination"
       result_hash[:procrastination] += 1
-    when "work"
+    when "working"
       result_hash[:work] += 1
     else
       result_hash[:count] += 1
@@ -30,14 +28,14 @@ def results
   procrastination_percent = (result_hash[:procrastination].to_f/result_hash[:count].to_f) * 100
   work_percent = (result_hash[:work].to_f/result_hash[:count].to_f) * 100
   puts "Play: #{play_percent.round(2)}% \nProcrastination: #{procrastination_percent.round(2)}% \nWork: #{work_percent.round(2)}%"
-  menu
 end
 
 def data_input
-  loop do
+  done = false
+  until done
     puts "I'm going to go take a nap, check in with you in a few!"
-    sleep 1
-    system("say hello")
+    # sleep 1
+    # system("say hello")
     puts "What are you doing?"
     puts "Choices: 1 for PLAY, 2 for PROCRASTINATION, 3 for WORKING, 4 for GO AWAY AND COME BACK LATER, 5 for PLEASE STOP"
     case gets.chomp
@@ -50,7 +48,7 @@ def data_input
     when "4"
       puts "Ok! I won't take any data this time."
     when "5"
-      menu
+      done = true
     else
       puts "that is not a valid response"
     end
@@ -61,17 +59,20 @@ def data_input
 end
 
 def menu
-  puts "What would you like to do?"
-  puts "Choices: 1 for Input, 2 for Results, 3 for Quit."
-  case gets.chomp
-  when "1"
-    data_input
-  when "2"
-    results
-  when "3"
-  else
-    puts "That wasn't a valid option, sorry!"
-    menu
+  done = false
+  until done
+    puts "What would you like to do?"
+    puts "Choices: 1 for Input, 2 for Results, 3 for Quit."
+    case gets.chomp
+    when "1"
+      data_input
+    when "2"
+      results
+    when "3"
+      done = true
+    else
+      puts "That wasn't a valid option, sorry!"
+    end
   end
 end
 
@@ -79,6 +80,7 @@ end
 # Actual Program
 puts "Welcome to your time tracker!"
 sleep 0.5
+
 menu
 
 puts "Thank you and Goodbye!!"

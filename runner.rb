38d587fd7284e47a45fa31479ccbfdbb2cca0238
deannawarren <mrs.deanna.warren@gmail.com>
2016-03-22 00:05:37 -0700

    Work on percentages, issue-- play percent has deced to be NaN?

--- runner.rb
+++ runner.rb
@@ -7,43 +7,57 @@ require 'date'
 def results
   puts "this will have the results"
   csv_output = CSV.read("data.csv")
-  csv_output.flatten!.shift
-  result_hash = {
-    play: 0,
-    procrastination: 0,
-    work: 0,
-    count: 0
-  }
-  csv_output.each_with_index do |value, index|
-    case value
-    when "play"
-      result_hash[:play] += 1
-    when "procrastination"
-      result_hash[:procrastination] += 1
-    when "working"
-      result_hash[:work] += 1
-    else
-      result_hash[:count] += 1
-      if Date.parse(value) == Date.today
-        case csv_output[index+1]
-        when "play"
-          result_hash[:today][:play] += 1
-        when "procrastination"
-          result_hash[:today][:procrastination] += 1
-        when "working"
-          result_hash[:today][:work] += 1
-        else
-          puts "There seems to be an error in your CSV file"
+  if csv_output[0] == nil
+    puts "There are no results!"
+  else
+    csv_output.flatten!
+    csv_output.shift
+    result_hash = {
+      today: {
+        play: 0,
+        procrastination: 0,
+        work: 0,
+        count: 0
+      },
+      play: 0,
+      procrastination: 0,
+      work: 0,
+      count: 0
+    }
+    csv_output.each_with_index do |value, index|
+      case value
+      when "play"
+        result_hash[:play] += 1
+      when "procrastination"
+        result_hash[:procrastination] += 1
+      when "working"
+        result_hash[:work] += 1
+      else
+        result_hash[:count] += 1
+        if Date.parse(value) == Date.today
+          result_hash[:today][:count] += 1
+          case csv_output[index+1]
+          when "play"
+            result_hash[:today][:play] += 1
+          when "procrastination"
+            result_hash[:today][:procrastination] += 1
+          when "working"
+            result_hash[:today][:work] += 1
+          else
+            puts "There seems to be an error in your CSV file"
+          end
+        end
       end
     end
+    play_percent = (result_hash[:play].to_f/result_hash[:count].to_f) * 100
+    p "#{((result_hash[:today][:play].to_f/result_hash[:today][:count].to_f) * 100)} - #{play_percent}"
+    procrastination_percent = (result_hash[:procrastination].to_f/result_hash[:count].to_f) * 100
+    work_percent = (result_hash[:work].to_f/result_hash[:count].to_f) * 100
+    play_change = ((result_hash[:today][:play].to_f/result_hash[:today][:count].to_f) * 100) - play_percent
+    procrastination_change = ((result_hash[:today][:procrastination].to_f/result_hash[:today][:count].to_f) * 100) - procrastination_change
+    work_change = ((result_hash[:today][:work].to_f/result_hash[:today][:count].to_f) * 100) - work_percent
+    puts "Play: #{play_percent.round(2)}% - #{play_change.round(2)}% change today\nProcrastination: #{procrastination_percent.round(2)}% - #{procrastination_change.round(2)}% change today \nWork: #{work_percent.round(2)}% - #{work_change.round(2)}% change today"
   end
-  play_percent = (result_hash[:play].to_f/result_hash[:count].to_f) * 100
-  procrastination_percent = (result_hash[:procrastination].to_f/result_hash[:count].to_f) * 100
-  work_percent = (result_hash[:work].to_f/result_hash[:count].to_f) * 100
-  play_change = ((result_hash[:today][:play].to_f/result_hash[:today][:count].to_f) * 100) - play_percent
-  procrastination_change = ((result_hash[:today][:procrastination].to_f/result_hash[:today][:count].to_f) * 100) - procrastination_change
-  work_change = ((result_hash[:today][:work].to_f/result_hash[:today][:count].to_f) * 100) - work_percent
-  puts "Play: #{play_percent.round(2)}% \nProcrastination: #{procrastination_percent.round(2)}% \nWork: #{work_percent.round(2)}%"
 end
 
 #--------------------------

1de5cc9d5d2f25613782f2fdac39286fe4d09abe
deannawarren <mrs.deanna.warren@gmail.com>
2016-03-22 23:25:50 -0700

    TimeTracker to class, start RSpec

---
+++ tracker_spec.rb
@@ -0,0 +1,19 @@
+
+describe "Time Tracker" do
+
+  context "Menu" do
+    it "starts the menu"
+    it "responds correctly for Input"
+    it "responds correctly for Results"
+    it "responds correctly for quit v1"
+    it "responds correctly for quit v2"
+  end # menu context
+
+  context "User Input" do
+    it "responds correctly to "
+  end # user input context
+
+  context "Results" do
+  end # results context
+
+end # describe Time Tracker
